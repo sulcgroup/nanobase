3cd0568f52bbe04ff00cd4e8ffcee068c8432cb4
Aatmik Mallya <aatmik54@gmail.com>
2020-10-12 21:39:31 -0700

    set up routing and add modules

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, make_response
+app = Flask(__name__, static_folder="ng/dist/ng", static_url_path="")
 
 print("TEST")
 
 @app.route('/')
-def hello_world():
-    # return "Hello"
-    return render_template('index.html')
+def home():
+    return make_response(open('ng/dist/ng/index.html').read())
 
-app.run(host="0.0.0.0", port=9000)
- 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=9000)
--- ng/src/app/app-routing.module.ts
+++ ng/src/app/app-routing.module.ts
@@ -10,22 +10,17 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'upload', loadChildren: () => import('./upload/upload.module').then(m => m.UploadModule) },
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    MatIconModule
-  ],
-  exports: [
-    RouterModule,
-    MatInputModule,
-    FormsModule,
-    MatButtonModule,
-    MatIconModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
