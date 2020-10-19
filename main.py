9ad4f3ead4ed7aa054cae28e5e16a002bd94c5aa
Aatmik Mallya <aatmik54@gmail.com>
2020-10-19 01:53:13 -0700

    set up design for the other modules

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, send_file, abort
+import os
 app = Flask(__name__, static_folder="ng/dist/ng", static_url_path="")
-
-print("TEST")
 
 @app.route('/')
 def home():
     return make_response(open('ng/dist/ng/index.html').read())
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return make_response(open('ng/dist/ng/index.html').read())
+
+@app.route("/images/<image>")
+def getImage(image=None):
+	if os.path.isfile("assets/images/{}".format(image)):
+		return send_file("assets/images/{}".format(image))
+	else:
+		abort(404, discription="Image not found")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9000)
--- ng/src/app/profile/profile.component.ts
+++ ng/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  hideOld = true;
+  hideNew = true;
+  oldPassword = new FormControl('', [Validators.required]);
+  newPassword = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getOldPasswordError(): string {
+    return this.oldPassword.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  getNewPasswordError(): string {
+    if (this.newPassword.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.newPassword.hasError('minlength') ? 'Must be at least 8 characters' : '';
   }
 
 }
