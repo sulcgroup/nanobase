a7ebacd2fec38b5015096a0a053d178e599c2aa6
Aatmik Mallya <aatmik54@gmail.com>
2020-10-24 18:02:33 -0700

    save

--- ng/src/app/upload/upload.module.ts
+++ ng/src/app/upload/upload.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UploadRoutingModule } from './upload-routing.module';
 import { UploadComponent } from './upload.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 
 
 @NgModule({
   declarations: [UploadComponent],
   imports: [
     CommonModule,
-    UploadRoutingModule
+    UploadRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatStepperModule
   ]
 })
 export class UploadModule { }
