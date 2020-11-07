df38e98863405c8d190f89e33509166c2f39cfd1
Aatmik Mallya <aatmik54@gmail.com>
2020-11-06 18:18:21 -0700

    change server script organization

--- server/vagrant_restart.sh
+++ server/vagrant_restart.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "Running main.py"
-#pkill -F /var/tmp/python.pid
 pkill gunicorn
-cd /var/nanobase/
+cd /vagrant/nanobase/server
 t=`date | tr -s ' ' | cut -d ' ' -f 2,3,4 | sed 's/ /_/g'`
 mv nohup.out logs/$t
 sh start.sh
-#echo $! > /var/tmp/python.pid
 chmod 664 nohup.out
