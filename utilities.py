d89e8d7e0445bbbeddac0cfff340188cf23b12ab
Aatmik Mallya <aatmik54@gmail.com>
2021-04-22 19:37:39 -0700

    change home path for server

--- utilities.py
+++ utilities.py
@@ -2,15 +2,15 @@ from flask import session
 
 
 def get_home_path():
-    return '/var/www/nanobase/nanobase/' if is_prod() else '/vagrant/nanobase/'
+    return '/var/www/nanobase.org/' if is_prod() else '/vagrant/nanobase/'
 
 def get_base_url():
-    return 'https://oxdna.org/' if is_prod() else 'http://localhost:8000/'
+    return 'https://nanobase.org/' if is_prod() else 'http://localhost:5000/'
 
 # Determine if we are in the server or the VM (true = server)
 def is_prod():
     try:
-    	open('/var/www/nanobase/nanobase/nanobase_email/login.txt', 'r')
+    	open('/var/www/nanobase.org/nanobase_email/login.txt', 'r')
     	return True
     except FileNotFoundError:
     	return False
