e590c339d18a2a6dec32bc180310af66bcbe1df0
Aatmik Mallya <aatmik54@gmail.com>
2021-05-18 17:23:14 -0700

    create loading bar for all pages

--- ng/src/app/core/services/index.ts
+++ ng/src/app/core/services/index.ts
@@ -2,4 +2,5 @@ export * from './api.service';
 export * from './form.service';
 export * from './user.service';
 export * from './structure.service';
+export * from './loadbar.service';
 export * from './auth.guard';
---
+++ ng/src/app/core/services/loadbar.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadbarService {
+  public loadBar = true;
+
+  constructor() { }
+
+  enable(): void {
+    this.loadBar = true;
+  }
+
+  disable(): void {
+    this.loadBar = false;
+  }
+
+  toggle(): void {
+    this.loadBar = !this.loadBar;
+  }
+}
