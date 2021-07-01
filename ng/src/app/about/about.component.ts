d5b6c86f457e7f711e66a72a45216e4d1d6d42af
AatmikMallya <AatmikMallya>
2021-07-01 01:52:54 +0000

    fixed click map on Chrome/Safari and disabled loadbar on reload

--- ng/src/app/about/about.component.ts
+++ ng/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadbarService } from '../core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public loadBarService: LoadbarService) { }
 
   ngOnInit(): void {
+    this.loadBarService.disable();
   }
 
 }
